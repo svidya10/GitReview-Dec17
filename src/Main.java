24b5ce5e28fd4e96d97284396176b680c635d4e4
svidya10 <svidya10@gmail.com>
2022-12-17 10:59:35 -0800

    version2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("version 1");
+        System.out.println("version 2");
     }
+    
 }
